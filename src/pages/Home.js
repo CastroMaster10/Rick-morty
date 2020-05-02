57274226943a02a530b106bb4a1c3bdfee06b483
CastroMaster10 <rudacate02@hotmail.com>
2020-05-02 01:02:14 -0500

    Se creo el proyecto con herramientas como babel y plugin de webpack

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    const view = `
+            <div class="Characters">
+            <article class="Character-Item">
+                <a hrfe="#/1/">
+                <img src="" alt="img-character"/>
+                <h2> </h2>
+                </a>
+            </article> 
+            </div>`;
+            return view;
+}
+
+export default Home;
---
+++ src/templates/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+
+    const view = 
+    `
+    <div class= "header-container">
+        <div class="header-logo" >
+            <h1>
+                <a href="/">
+                    cinetifico
+                </a>
+            </h1>
+        </div>
+        <div class="navbar">
+            <a href="#/about/">About</a>
+            
+        </div>
+    </div>
+    `
+    return view;
+}
+
+export default Header;
